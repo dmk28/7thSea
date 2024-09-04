83f45e1ff876cb57efbf174263292db2dc4a142b
Daniel K <daniel.kik.adv@protonmail.com>
2024-09-04 14:43:05 -0300

    removed a change, will find a better way later

--- world/combat_script/combat_system.py
+++ world/combat_script/combat_system.py
@@ -1141,8 +1141,7 @@ class CombatScript(DefaultScript):
         character.ndb.stop_thrust = False
         if 'nosestruck' in character.ndb.special_effects:
             character.ndb.special_effects.remove('facestruck')
-        if 'defending' in character.ndb.special_effects:
-            character.ndb.special_effects.remove('defending')
+
 
 
     def force_end_combat(self):
