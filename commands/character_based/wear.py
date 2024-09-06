1b24897069163613da318f543eed0a04a0988c50
Daniel K <daniel.kik.adv@protonmail.com>
2024-09-06 00:13:35 -0300

    wear

--- commands/character_based/wear.py
+++ commands/character_based/wear.py
@@ -1,8 +1,8 @@
 from evennia import Command, CmdSet
 from evennia.utils.utils import inherits_from
-from typeclasses.armor.armor_and_clothes import Armor, DracheneisenArmor
 from evennia.commands.default.muxcommand import MuxCommand
 from typeclasses.objects import Armor
+from evennia.utils.evmenu import EvMenu
 
 class CmdWear(MuxCommand):
     """
@@ -18,6 +18,7 @@ class CmdWear(MuxCommand):
     key = "wear"
     locks = "cmd:all()"
     switches = ["examine"]
+
     def func(self):
         caller = self.caller
         if not self.args:
@@ -45,23 +46,42 @@ class CmdWear(MuxCommand):
             self.caller.msg(f"|cDetails:|n {item.db.details}")
         self.caller.msg(f"|cArmor Value:|n {item.db.armor}")
         self.caller.msg(f"|cSoak Keep:|n {item.db.soak_keep}")
-        self.caller.msg(f"|cWear Location:|n {', '.join(item.db.wear_location) if isinstance(item.db.wear_location, list) else item.db.wear_location}")
+        self.caller.msg(f"|cWear Location(s):|n {', '.join(item.db.wear_location)}")
 
     def wear_item(self):
-        if not location and isinstance(item.db.wear_location, list):
-            location = item.db.wear_location[0]
-        elif not location:
-            location = item.db.wear_location
+        item = self.caller.search(self.args, location=self.caller)
+        if not item:
+            return
 
-        if location in self.caller.db.worn_items:
-            self.caller.msg(f"You are already wearing something on your {location}.")
-            return False
+        if not isinstance(item, Armor):
+            self.caller.msg("That's not a piece of armor or clothing.")
+            return
 
-        if item.wear(self.caller):
-            self.caller.db.worn_items[location] = item
-            self.caller.calc_total_armor()
-            return True
-        return False
+        wear_locations = item.db.wear_location
+        if len(wear_locations) > 1:
+            # If the item can be worn in multiple locations, start a menu to choose
+            self.caller.ndb._wear_choice = item
+            EvMenu(self.caller, "commands.wear", "wear_location_menu",
+                   item=item, wear_locations=wear_locations)
+        else:
+            self.do_wear(item, wear_locations[0])
+
+    def do_wear(self, item, location):
+        caller = self.caller
+        if not hasattr(caller.db, 'worn_items'):
+            caller.db.worn_items = {}
+
+        if location in caller.db.worn_items:
+            caller.msg(f"You are already wearing something on your {location}.")
+            return
+
+        if item.wear(caller):
+            caller.db.worn_items[location] = item
+            caller.calc_total_armor()
+            caller.msg(f"You wear {item.name} on your {location}.")
+            caller.location.msg_contents(f"{caller.name} wears {item.name} on their {location}.", exclude=caller)
+        else:
+            caller.msg(f"You can't wear {item.name}.")
 
 class CmdRemove(Command):
     """
@@ -87,48 +107,47 @@ class CmdRemove(Command):
                 if item.remove():
                     del caller.db.worn_items[location]
                     caller.calc_total_armor()
-                    caller.msg(f"You remove {item.name}.")
-                    caller.location.msg_contents(f"{caller.name} removes {item.name}.", exclude=caller)
+                    caller.msg(f"You remove {item.name} from your {location}.")
+                    caller.location.msg_contents(f"{caller.name} removes {item.name} from their {location}.", exclude=caller)
                 else:
                     caller.msg(f"You can't remove {item.name}.")
                 return
 
         caller.msg(f"You're not wearing {item_name}.")
 
-# This is a new command to handle multi-location wear choices
-class CmdWearChoice(Command):
-    """
-    Hidden command to handle wear location choices for multi-location items.
-    """
-    key = "wear_choice"
-    locks = "cmd:all()"
+def wear_location_menu(caller, raw_string, **kwargs):
+    item = kwargs.get("item")
+    wear_locations = kwargs.get("wear_locations")
 
-    def func(self):
-        caller = self.caller
-        item = caller.ndb._wear_choice
-        if not item:
-            caller.msg("Wear what?")
-            return
+    if not item or not wear_locations:
+        return "exit"
 
-        try:
-            choice = int(self.args) - 1
-            location = item.db.wear_location[choice]
-        except (ValueError, IndexError):
-            caller.msg("Invalid choice.")
-            del caller.ndb._wear_choice
-            return
+    text = f"Where do you want to wear {item.name}?\n"
+    options = []
 
-        if CmdWear.wear_item(self, item, location):
-            caller.msg(f"You wear {item.name} on your {location}.")
-            caller.location.msg_contents(f"{caller.name} wears {item.name} on their {location}.", exclude=caller)
-        else:
-            caller.msg(f"You can't wear {item.name} on your {location}.")
+    for i, location in enumerate(wear_locations, 1):
+        text += f"{i}. {location}\n"
+        options.append({"key": str(i), "desc": location, "goto": "do_wear_from_menu"})
 
-        del caller.ndb._wear_choice
+    options.append({"key": "q", "desc": "Quit", "goto": "exit"})
 
+    return text, options
+
+def do_wear_from_menu(caller, raw_string, **kwargs):
+    item = kwargs.get("item")
+    wear_locations = kwargs.get("wear_locations")
+
+    try:
+        choice = int(raw_string) - 1
+        location = wear_locations[choice]
+    except (ValueError, IndexError):
+        caller.msg("Invalid choice.")
+        return "wear_location_menu"
+
+    CmdWear.do_wear(CmdWear(), item, location)
+    return "exit"
 
 class WearCmdset(CmdSet):
     def at_cmdset_creation(self):
         self.add(CmdWear())
         self.add(CmdRemove())
-        self.add(CmdWearChoice())
